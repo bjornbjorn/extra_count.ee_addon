c6c0a84f042e64f8db2b6d108b2a70a7ff9769b0
Bjørn Børresen <bjorn@lastfriday.no>
2014-03-25 12:39:21 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-extra_count.ee_addon
+Extra Count
 ====================
 
 {count} being overridden within a tag? Not any more - with the AWESOME POWER of {extra:count} !!
+
+Extra Count will add a variable called {extra:count} to all {exp:channel:entries} tags that hopefully won't be overridden.
+
+Fixes this problem:
+
+```
+{exp:ee_addon:tag}
+
+	{exp:channel:entries channel="blog" limit="10"}
+		Oh no {count} is always 1 due to EE's template parser! .. {extra:count} to the rescue :-)
+	{/exp:channel:entries}
+
+{/exp:ee_addon:tag}
+
+```
---
+++ system/expressionengine/third_party/extra_count/ext.extra_count.php
@@ -0,0 +1,128 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * ExpressionEngine - by EllisLab
+ *
+ * @package		ExpressionEngine
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2003 - 2011, EllisLab, Inc.
+ * @license		http://expressionengine.com/user_guide/license.html
+ * @link		http://expressionengine.com
+ * @since		Version 2.0
+ * @filesource
+ */
+ 
+// ------------------------------------------------------------------------
+
+/**
+ * Extra Count Extension
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Addons
+ * @category	Extension
+ * @author		Bjørn Børresen
+ * @link		http://wedoaddons.com
+ */
+
+class Extra_count_ext {
+	
+	public $settings 		= array();
+	public $description		= 'Adds an {extra:count} variable that is the same as {count} but should not be overwritten';
+	public $docs_url		= 'http://wedoaddons.com';
+	public $name			= 'Extra Count';
+	public $settings_exist	= 'n';
+	public $version			= '1.0';
+	
+	private $EE;
+	
+	/**
+	 * Constructor
+	 *
+	 * @param 	mixed	Settings array or empty string if none exist.
+	 */
+	public function __construct($settings = '')
+	{
+		$this->EE =& get_instance();
+		$this->settings = $settings;
+	}// ----------------------------------------------------------------------
+	
+	/**
+	 * Activate Extension
+	 *
+	 * This function enters the extension into the exp_extensions table
+	 *
+	 * @see http://codeigniter.com/user_guide/database/index.html for
+	 * more information on the db class.
+	 *
+	 * @return void
+	 */
+	public function activate_extension()
+	{
+		// Setup custom settings in this array.
+		$this->settings = array();
+		
+		$data = array(
+			'class'		=> __CLASS__,
+			'method'	=> 'on_channel_entries_row',
+			'hook'		=> 'channel_entries_row',
+			'settings'	=> serialize($this->settings),
+			'version'	=> $this->version,
+			'enabled'	=> 'y'
+		);
+
+		$this->EE->db->insert('extensions', $data);			
+		
+	}	
+
+	// ----------------------------------------------------------------------
+	
+	/**
+	 * on_channel_entries_row
+	 *
+	 * @param 
+	 * @return 
+	 */
+	public function on_channel_entries_row($ref, $row)
+	{
+        $row['extra:count'] = $row['count'];
+        return $row;
+	}
+
+	// ----------------------------------------------------------------------
+
+	/**
+	 * Disable Extension
+	 *
+	 * This method removes information from the exp_extensions table
+	 *
+	 * @return void
+	 */
+	function disable_extension()
+	{
+		$this->EE->db->where('class', __CLASS__);
+		$this->EE->db->delete('extensions');
+	}
+
+	// ----------------------------------------------------------------------
+
+	/**
+	 * Update Extension
+	 *
+	 * This function performs any necessary db updates when the extension
+	 * page is visited
+	 *
+	 * @return 	mixed	void on update / false if none
+	 */
+	function update_extension($current = '')
+	{
+		if ($current == '' OR $current == $this->version)
+		{
+			return FALSE;
+		}
+	}	
+	
+	// ----------------------------------------------------------------------
+}
+
+/* End of file ext.extra_count.php */
+/* Location: /system/expressionengine/third_party/extra_count/ext.extra_count.php */
